5a2fbf054534ffb257bd2c934d7801c3f57f8f46
AbdallahRdf <abdallahradfi@gmail.com>
2024-12-29 18:27:35 +0100

    Add authentication routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,17 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
+use App\Controllers\User;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+// show login and signup pages
+$routes->get('/login', [User::class, 'showLoginForm']);
+$routes->get('/signup', [User::class, 'showSignupForm']);
+
+// handle login and signup requests
+$routes->post('/login', [User::class, 'login']);
+$routes->post('/signup', [User::class, 'signup']);
