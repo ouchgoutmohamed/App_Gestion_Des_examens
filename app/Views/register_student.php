eef0a758ca0bd59ff6b0e6a23a988dc49aeebd07
IDOUFKIR Kamal <idoufkir.kamal@gmail.com>
2024-12-30 13:44:18 +0100

    Add login UI

--- app/Views/register_student.php
+++ app/Views/register_student.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <title>Student Registration | ExamManager</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="Gestion des examens" name="description">
+    <meta content="App de gestion des examens" name="description">
 
     <!-- App favicon -->
     <link rel="shortcut icon" href="public/assets/images/favicon.ico">
