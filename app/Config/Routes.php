36d5eba7b74df3cf3430e16f8f4c42e0d251bbcd
AbdallahRdf <abdallahradfi@gmail.com>
2025-01-14 22:47:16 +0100

    Add Guest filter, so if user is logged in, he can't access login or signup pages

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,20 +7,18 @@ use App\Controllers\UserController;
 use App\Controllers\StudentController;
 use App\Enums\Roles;
 
-
 /**
  * @var RouteCollection $routes
  */
 $routes->addRedirect('/', '/login');
 
 // show login and signup pages
-$routes->view('/login', 'login');
-$routes->view('/signup', 'register_student');
+$routes->view('/login', 'login', ['filter' => 'guest']);
+$routes->view('/signup', 'register_student', ['filter' => 'guest']);
 
 // handle login and signup requests
-$routes->post('/login', [UserController::class, 'login']);
-$routes->post('/signup', [StudentController::class, 'signup']);
-
+$routes->post('/login', [UserController::class, 'login'], ['filter' => 'guest']);
+$routes->post('/signup', [StudentController::class, 'signup'], ['filter' => 'guest']);
 
 // goup of routes with auth filter
 $routes->group('', ['filter' => 'auth'], function ($routes) {
@@ -29,7 +27,6 @@ $routes->group('', ['filter' => 'auth'], function ($routes) {
     $routes->group('', ['filter' => 'role:' . Roles::PROFESSOR->value], function ($routes) {
         // show professor's dashboard
         $routes->view('/professor_dashboard', 'professor/professor_dashboard');
-
 
         // gets the courses that are taught by the logged in professor
         $routes->get('/grades_management', [CourseController::class, 'get_courses']);
@@ -40,13 +37,16 @@ $routes->group('', ['filter' => 'auth'], function ($routes) {
 
     // studnet's routes
     $routes->group('', ['filter' => 'role:' . Roles::STUDENT->value], function ($routes) {
-        // handle logout request
-        $routes->get('/logout', [UserController::class, 'logout']);
 
         // show student's dashboard
         $routes->view('/student_dashboard', 'student/student_dashboard');
 
         $routes->view('/results', 'student/results');
     });
+
+    // handle logout request
+    $routes->get('/logout', [UserController::class, 'logout']);
+
+    // show unauthorized page (403 status code)
+    $routes->view('/unauthorized', 'unauthorized');
 });
-$routes->view('/unauthorized', 'unauthorized');
