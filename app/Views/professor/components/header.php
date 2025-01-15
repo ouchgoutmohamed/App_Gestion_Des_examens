bbc85a827fd4be189d28a540e5fcba6d8bcef234
IDOUFKIR Kamal <idoufkir.kamal@gmail.com>
2025-01-15 13:33:02 +0100

    Import grades from excel file (Frontend)

--- app/Views/professor/components/header.php
+++ app/Views/professor/components/header.php
@@ -11,11 +11,11 @@
                 <!-- Profile Dropdown Button -->
                 <div class="relative">
                     <button data-fc-type="dropdown" data-fc-placement="bottom-end" type="button" class="nav-link">
-                        <img src="assets/images/users/user-6.jpg" alt="user-image" class="rounded-full h-10">
+                        <img src="<?= base_url('assets/images/users/user-6.jpg') ?>" alt="user-image" class="rounded-full h-10">
                     </button>
                     <div class="fc-dropdown fc-dropdown-open:opacity-100 hidden opacity-0 w-44 z-50 transition-[margin,opacity] duration-300 mt-2 bg-white shadow-lg border rounded-lg p-2 border-gray-200 dark:border-gray-700 dark:bg-gray-800">
                         <a class="flex items-center py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300" href="javascript:void(0)">
-                            <i class="mgc_pic_2_line  me-2"></i> 
+                            <i class="mgc_user_3_line me-2"></i> 
                             <span>Profile</span>
                         </a>
                         <hr class="my-2 -mx-2 border-gray-200 dark:border-gray-700">
